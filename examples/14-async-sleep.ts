6ee3707707bc9d037ed7795f1498f860f48baa36
David Pesta <davidpesta@gmail.com>
2022-06-01 06:42:17 -0500

    Added async sleep example.

---
+++ examples/14-async-sleep.ts
@@ -0,0 +1,11 @@
+import easy from "https://deno.land/x/easyutil@0.6.0/mod.ts";
+import { serve, EasyRequest, EasyResponse } from "https://deno.land/x/easyroute@0.1.0/mod.ts";
+
+async function sleepForSeconds(_request: EasyRequest, seconds: number): Promise<EasyResponse> {
+	await easy.sleep(seconds * 1000);
+	return new EasyResponse("Done!");
+}
+
+serve({
+	"GET /sleep/seconds:number": sleepForSeconds
+});
